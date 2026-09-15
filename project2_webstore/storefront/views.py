9a528f3fc6b88b29436422bb07517e4ec162ee2e
amusinski <amusinski@gmail.com>
2026-09-14 20:08:48 -0500

    Made Visual Changes: Adjusted Hero to show company tagline instead of name to keep from over branding without having the company motto. I added the same color autoamtion to the cards. I added the 'Honk' font-style. Just trying it out.

--- project2_webstore/storefront/views.py
+++ project2_webstore/storefront/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 def landing(request):
     context = {
         'store_name': 'Little Apple Nutrition',
-        'tagline': '',
+        'tagline': 'Little Apple, Big Impact',
     }
     return render(request, 'storefront/landing.html', context)
